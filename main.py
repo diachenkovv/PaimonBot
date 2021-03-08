eacaf16bf79dbdadccada400bf20cc47fe51885f
Kazuto <dyachenkoviktor1999@gmail.com>
2021-03-08 12:22:52 +0200

    Добавлен аргумент config.py, закоментирован код

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ bot = commands.Bot(
     command_prefix=settings['prefix'], intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 guild_ids = [settings['guild_ids']]
-# Не передаём аргумент pass_context, так как он был нужен в старых версиях.
 
-
-@slash.slash(name="test")
-async def _test(ctx: SlashContext):
-    embed = discord.Embed(title="embed test")
-    await ctx.send(content="test", embeds=[embed])
+# инициализация бота
 
 
 @bot.event
 async def on_ready():
     print("On Air!")
 
+# текст формата embed
+
+
+@slash.slash(name="embed", guild_ids=guild_ids)
+async def _test(ctx: SlashContext):
+    embed = discord.Embed(title="embed test")
+    await ctx.send(content="test", embeds=[embed])
+
+# отправка введенного аргумента
+
 
 @slash.slash(
-    name="test",
+    name="repeat",
     description="this returns the bot latency",
     options=[manage_commands.create_option(
         name="argone",
@@ -37,26 +42,32 @@ async def _test(ctx, argone: str):
     await ctx.respond()
     await ctx.send(f"You responded with {argone}.")
 
+# отправка текста "test"
 
-@slash.slash(name="test")
+
+@slash.slash(name="text", guild_ids=guild_ids)
 async def test(ctx):
     await ctx.respond()
     await ctx.send('test')
 
+# отправка invite ссылки
 
-@slash.slash(name="invite")
+
+@slash.slash(name="invite", guild_ids=guild_ids)
 async def invite(ctx):
-    await ctx.respond()
     await ctx.send("Эй!\nЕсли хочешь добавить меня на свой сервер, то вот ссылка: [Paimon Bot](https://discordapp.com/oauth2/authorize?&client_id=818187894891610182&scope=bot&permissions=8)")
 
+# отправка сообщения "Hello, {имя учасника с упоминанием}"
 
-@slash.slash(name="hello")
+
+@slash.slash(name="hello", guild_ids=guild_ids)
 async def hello(ctx):  # Создаём функцию и передаём аргумент ctx.
     # Объявляем переменную author и записываем туда информацию об авторе.
     author = ctx.message.author
-    await ctx.respond()
     # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
     await ctx.send(f'Hello, {author.mention}!')
 
 # Обращаемся к словарю settings с ключом token, для получения токена
 bot.run(settings['token'])
+
+# разработка слеш команд временно приостановлена в связи с нестабильной работой библиотеки
